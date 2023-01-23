2aa944bcb9e8ba7f957cd0833a303f3834d85cb2
Léo <leo.muller@aidalinfo.fr>
2023-01-23 14:09:17 +0100

    update

--- src/exportFromJSON.js
+++ src/exportFromJSON.js
@@ -3,11 +3,13 @@ import { downloadFile, generateDataURI } from './processors.js';
 import { _prepareData, _createJSONData, createCSVData, createXLSData, createXMLData, _createFieldsMapper } from './converters.js';
 import { exportTypes } from './types.js';
 function exportFromJSON(_a) {
-    let data = _a.data, _b = _a.fileName, fileName = _b === void 0 ? 'download' : _b, extension = _a.extension, _c = _a.fileNameFormatter, fileNameFormatter = _c === void 0 ? function (name) { return name.replace(/\s+/, '_'); } : _c, fields = _a.fields, _d = _a.exportType, exportType = _d === void 0 ? 'txt' : _d, _e = _a.replacer, replacer = _e === void 0 ? null : _e, _f = _a.space, space = _f === void 0 ? 4 : _f, _g = _a.processor, processor = _g === void 0 ? downloadFile : _g, _h = _a.withBOM, withBOM = _h === void 0 ? false : _h, _j = _a.beforeTableEncode, beforeTableEncode = _j === void 0 ? function (i) { return i; } : _j;
+    let data = _a.data, _b = _a.fileName, fileName = _b === void 0 ? 'download' : _b, extension = _a.extension, _c = _a.fileNameFormatter, fileNameFormatter = _c === void 0 ? function (name) { return name.replace(/\s+/, '_'); } : _c, fields = _a.fields, _d = _a.exportType, exportType = _d === void 0 ? 'txt' : _d, _e = _a.replacer, replacer = _e === void 0 ? null : _e, _f = _a.space, space = _f === void 0 ? 4 : _f, _g = _a.processor, processor = _g === void 0 ? downloadFile : _g, _h = _a.withBOM, withBOM = _h === void 0 ? false : _h, _j = _a.beforeTableEncode, beforeTableEncode = _j === void 0 ? function (i) { return i; } : _j, _k = _a.fileWithoutDownload, fileWithoutDownload = _k === void 0 ? false : _k;
     let MESSAGE_IS_ARRAY_FAIL = 'Invalid export data. Please provide an array of objects';
     let MESSAGE_UNKNOWN_EXPORT_TYPE = "Can't export unknown data type ".concat(exportType, ".");
     let MESSAGE_FIELD_INVALID = "Can't export string data to ".concat(exportType, ".");
-    _a.fileWithoutDownload ? _a.processor = generateDataURI : null;
+    
+    fileWithoutDownload ? processor = generateDataURI : null;
+
     if (typeof data === 'string') {
         switch (exportType) {
             case 'txt':
